5264b624fbc6bb50413a41fdfe6d37941d5eca31
Ahad Islam <ahad.islam624@gmail.com>
2020-10-09 10:38:51 -0400

    Created a function for signing a user up with firebase

--- services/firebaseAuth.js
+++ services/firebaseAuth.js
@@ -18,3 +18,19 @@ export function signIn(username, password, navigation) {
       console.error(error);
     });
 }
+
+export function signUp(username, password, navigation) {
+  auth
+  .createUserWithEmailAndPassword(
+    username,
+    password
+  )
+  .then(() => {
+    console.log("User signed up! Navigating to Dashboard...");
+    navigation.navigate("Dashboard", { name: "Dashboard" });
+  })
+  .catch((error) => {
+
+    console.error(error);
+  });
+}
